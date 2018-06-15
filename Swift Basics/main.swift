f2592c35bb2efbe9531087cc0d6b9b4635f3507f
yen <984853386@qq.com>
2018-06-16 06:43:14 +0800

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by 杨雁 on 6/16/18.
+//  Copyright © 2018 Tech innovator. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
